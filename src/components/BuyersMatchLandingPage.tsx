0a8f0f2a9a179a17b249416f690f4bde46cf3084
CandidateMaster2002 <kickmegreen@gmail.com>
2026-02-10 12:16:27 +0530

    google rev in mob issue res

--- src/components/BuyersMatchLandingPage.tsx
+++ src/components/BuyersMatchLandingPage.tsx
@@ -25,7 +25,7 @@ const SectionWrapper = ({ children }: { children: React.ReactNode }) => (
     <motion.div
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, margin: "-100px", amount: 0.1 }}
+        viewport={{ once: true, amount: 0.05 }}
         transition={{
             duration: 0.5
         }}
